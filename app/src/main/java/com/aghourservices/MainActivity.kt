43365a67fe1da7c8aa413f0a6ad326e9fa8518f2
m7madmagdy <mohamedmagdysaber@gmail.com>
2021-08-08 12:43:29 +0200

    Create Simple Recycler View

--- app/src/main/java/com/aghourservices/MainActivity.kt
+++ app/src/main/java/com/aghourservices/MainActivity.kt
@@ -2,81 +2,58 @@ package com.aghourservices
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONArrayRequestListener
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import org.json.JSONArray
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidNetworking.initialize(getApplicationContext());
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
 
-        fab.setOnClickListener { view ->
-            makeRequest()
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..5) {
+            data.add(ItemsViewModel(R.drawable.messi, "Item $i"))
+            data.add(ItemsViewModel(R.drawable.messi, "Item $i"))
+            data.add(ItemsViewModel(R.drawable.messi, "Item $i"))
+            data.add(ItemsViewModel(R.drawable.messi, "Item $i"))
+            data.add(ItemsViewModel(R.drawable.messi, "Item $i"))
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
 
     }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    fun makeRequest() {
+    private fun makeRequest() {
         AndroidNetworking.get("https://jsonplaceholder.typicode.com/todos/")
-                .setPriority(Priority.LOW)
-                .build()
-                .getAsJSONArray(object : JSONArrayRequestListener {
-                    override fun onResponse(response: JSONArray?) {
-                        Log.v("Response", response.toString());
-                    }
+            .setPriority(Priority.LOW)
+            .build()
+            .getAsJSONArray(object : JSONArrayRequestListener {
+                override fun onResponse(response: JSONArray?) {
+                    Log.v("Response", response.toString());
+                }
 
-                    override fun onError(error: ANError?) {
-                        // handle error
-                        Log.v("Response", error.toString());
-                    }
-                })
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+                override fun onError(error: ANError?) {
+                    // handle error
+                    Log.v("Response", error.toString());
+                }
+            })
     }
 }
---
+++ app/src/main/java/com/aghourservices/itemViewModel.kt
@@ -0,0 +1,4 @@
+package com.aghourservices
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
