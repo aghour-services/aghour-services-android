922b33dc92e6443cf15da5972d18bd615e461e49
m7madmagdy <mohamedmagdysaber@gmail.com>
2021-08-09 17:46:27 +0200

    Recycler View With Toolbar

--- app/src/main/java/com/aghourservices/SplashScreen.kt
+++ app/src/main/java/com/aghourservices/SplashScreen.kt
@@ -1,12 +1,11 @@
 package com.aghourservices
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.Window
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.aghourservices.categories.MainActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +16,6 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
-
+        }, 300)
     }
 }
--- app/src/main/java/com/aghourservices/itemViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.aghourservices
-
-data class ItemsViewModel(val image: Int, val text: String) {
-}
