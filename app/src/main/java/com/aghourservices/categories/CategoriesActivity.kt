0330c0dbd98410f65f9e09c1ebb202828263ee80
m7madmagdy <mohamedmagdysaber@gmail.com>
2021-08-10 13:26:55 +0200

    Added click action on items view

--- app/src/main/java/com/aghourservices/categories/CategoriesActivity.kt
+++ app/src/main/java/com/aghourservices/categories/CategoriesActivity.kt
@@ -1,26 +1,34 @@
 package com.aghourservices.categories
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import com.aghourservices.ListActivity
 import com.aghourservices.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.my_toolbar.*
 
 
 class CategoriesActivity : AppCompatActivity() {
+    val data = ArrayList<ItemsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(myToolbar)
 
         recyclerview.layoutManager = GridLayoutManager(this, 2)
-        val data = ArrayList<ItemsViewModel>()
-        val name = "Leo Messi"
-        for (i in 0..19) {
-            data.add(ItemsViewModel(R.drawable.messi, name + (i + 1)))
+        val messi = "Leo Messi"
+        for (i in 0..9) {
+            data.add(ItemsViewModel(R.drawable.messi, messi + (i + 1)))
         }
-        val adapter = CustomAdapter(data)
+        val adapter = CustomAdapter(data) { position -> onListItemClick(position) }
         recyclerview.adapter = adapter
+    }
+
+    private fun onListItemClick(position: Int) {
+        val intent = Intent(this, ListActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/aghourservices/listAdapter.kt
+++ app/src/main/java/com/aghourservices/listAdapter.kt
@@ -1,20 +1,21 @@
 package com.aghourservices
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.listdesign.view.*
-import kotlinx.android.synthetic.main.recycler_view_design.view.*
 
 class listAdapter(context: Context,arrlist:ArrayList<list_data>)
     :ArrayAdapter<list_data>(context,0,arrlist) {
+    @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val data=LayoutInflater.from(context).inflate(R.layout.listdesign,parent,false)
          val items=getItem(position)
         data.txt1.text=items!!.name
-        data.txt2.text=items.desc
+        data.txt2.text= items.desc
         data.circleImageView.setImageResource(items.img)
         return data
     }
