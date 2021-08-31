1ab2564bf867386459c9aa4c66de094010fb3fbd
Mohamed Magdy <52765461+m7madmagdy@users.noreply.github.com>
2021-08-31 10:13:34 +0200

    add app icon and change gradle version to 7.1.0-alpha07 (#32)
    * fix gradle
    
    * fix app icon && change gradle version to alpha07
    
    Co-authored-by: m7madmagdy <m7madmagdy@github.com>

--- app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
@@ -4,10 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.aghourservices.R
 import com.aghourservices.categories.CategoriesActivity
@@ -21,6 +17,6 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, CategoriesActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 4000)
     }
 }
