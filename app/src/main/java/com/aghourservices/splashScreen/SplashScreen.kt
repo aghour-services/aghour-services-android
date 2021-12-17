9b4c652746552ce93de5278cafdabfbeb2cf2ccc
m7madmagdy <mohamedmagdysaber@gmail.com>
2021-12-18 01:22:27 +0200

    customize splash screen

--- app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
@@ -6,28 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import com.aghourservices.R
 import com.aghourservices.cache.UserInfo
 import com.aghourservices.categories.CategoriesActivity
+import com.aghourservices.databinding.ActivitySplashScreenBinding
 import com.aghourservices.user.SignupActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            lateinit var intent: Intent
-            val userInfo = UserInfo()
-
-            if (userInfo.isUserLoggedIn(this)) {
-                intent = Intent(this, CategoriesActivity::class.java)
-            } else {
-                intent = Intent(this, SignupActivity::class.java)
-            }
-            startActivity(intent)
-            finish()
-        }, 1000)
+            Handler(Looper.getMainLooper()).postDelayed({
+                lateinit var intent: Intent
+                val userInfo = UserInfo()
+                intent = if (userInfo.isUserLoggedIn(this)) {
+                    Intent(this, CategoriesActivity::class.java)
+                } else {
+                    Intent(this, SignupActivity::class.java)
+                }
+                startActivity(intent)
+                finish()
+            }, 400)
+        }, 800)
     }
 }
