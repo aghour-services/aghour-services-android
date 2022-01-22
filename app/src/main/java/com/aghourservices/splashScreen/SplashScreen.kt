8572f593edfababdf24fda22bd4146b697643747
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-01-22 20:04:27 +0200

    Added News Subscription

--- app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/aghourservices/splashScreen/SplashScreen.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aghourservices.MainActivity
 import com.aghourservices.R
@@ -13,6 +15,8 @@ import com.aghourservices.cache.Settings
 import com.aghourservices.cache.UserInfo
 import com.aghourservices.databinding.ActivitySplashScreenBinding
 import com.aghourservices.user.SignUpActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
@@ -22,7 +26,14 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Firebase.messaging.subscribeToTopic("News")
+            .addOnCompleteListener { task ->
+                var msg = "Done"
+                if (!task.isSuccessful) {
+                    msg = "Failed"
+                }
+                Log.d("FCM", msg)
+            }
         Handler(Looper.getMainLooper()).postDelayed({
             lateinit var intent: Intent
             val userInfo = UserInfo()
@@ -36,7 +47,7 @@ class SplashScreen : AppCompatActivity() {
             }
             startActivity(intent)
             finish()
-            overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }, 600)
     }
 }
