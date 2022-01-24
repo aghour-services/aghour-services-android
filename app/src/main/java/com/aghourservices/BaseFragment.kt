eeb3cd457907445495bfcfa919816b3b509dd285
m7madmagdy <mohamedmagdysaber@gmail.com>
2022-01-24 03:19:45 +0200

    firms scroll to Top

--- app/src/main/java/com/aghourservices/BaseFragment.kt
+++ app/src/main/java/com/aghourservices/BaseFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import com.aghourservices.ads.Interstitial
 import com.aghourservices.firebase_analytics.Event
 
-open class BaseFragment : Fragment() {
+open class BaseFragment : Fragment(), ActivityFragmentCommunicator {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Event.sendScreenName(this::class.simpleName.toString())
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
     }
 }
