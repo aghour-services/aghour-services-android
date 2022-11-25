eba0813d1fa2ce9a1b8edead3b3e812563e9abdb
m7madmagdy <mohamedmagdysaber@gmail.com>
2022-11-25 18:40:49 +0200

    Show User Name in comments

--- app/src/main/java/com/aghourservices/ui/adapter/CommentsViewHolder.kt
+++ app/src/main/java/com/aghourservices/ui/adapter/CommentsViewHolder.kt
@@ -1,24 +1,17 @@
 package com.aghourservices.ui.adapter
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.aghourservices.data.model.Article
 import com.aghourservices.data.model.Comment
 import com.aghourservices.databinding.CommentCardBinding
-import com.aghourservices.databinding.NewsCardBinding
-import com.aghourservices.ui.main.cache.UserInfo.getUserData
-import com.aghourservices.utils.helper.Intents.copyNews
-import com.aghourservices.utils.helper.Intents.shareNews
 
 class CommentsViewHolder(
     val binding: CommentCardBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun setCommentView(comment: Comment) {
-        val user = getUserData(binding.root.context)
         binding.apply {
             body.text = comment.body
-            userName.text = user.name
+            userName.text = comment.user?.name
         }
     }
 }
