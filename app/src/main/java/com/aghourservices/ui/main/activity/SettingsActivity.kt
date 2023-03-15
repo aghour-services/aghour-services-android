a65a6252aadef2cf69e2544204b2704ae28a5a62
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-03-15 20:39:42 +0200

    Get user profile

--- app/src/main/java/com/aghourservices/ui/main/activity/SettingsActivity.kt
+++ app/src/main/java/com/aghourservices/ui/main/activity/SettingsActivity.kt
@@ -1,12 +1,15 @@
 package com.aghourservices.ui.main.activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.isVisible
 import com.aghourservices.R
+import com.aghourservices.data.model.Profile
+import com.aghourservices.data.request.RetrofitInstance
 import com.aghourservices.databinding.ActivitySettingsBinding
 import com.aghourservices.ui.main.cache.UserInfo.getUserData
 import com.aghourservices.ui.main.cache.UserInfo.isUserLoggedIn
@@ -20,22 +23,25 @@ import com.aghourservices.utils.helper.Intents.showOnCloseDialog
 import com.aghourservices.utils.helper.Intents.whatsApp
 import com.aghourservices.utils.helper.ThemePreference
 import com.google.android.gms.ads.AdView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
     private lateinit var adView: AdView
+    private val user by lazy { getUserData(this) }
+    private val isUserLogin by lazy { isUserLoggedIn(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         adView()
-        checkUser()
-        hideUserLogOut()
         initUserClick()
     }
 
-    private fun adView(){
+    private fun adView() {
         adView = findViewById(R.id.adView)
         Banner.show(this, adView)
     }
@@ -74,13 +80,18 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun checkUser() {
-        val user = getUserData(this)
-        if (isUserLoggedIn(this)) {
-            binding.btnRegister.visibility = View.GONE
-            binding.userDataView.visibility = View.VISIBLE
-            binding.userName.text = user.name
-            binding.userMobile.text = user.mobile
-            binding.userEmail.text = user.email
+        if (isUserLogin) {
+            binding.apply {
+                btnRegister.isVisible = false
+                userDataView.isVisible = true
+                logOut.isVisible = true
+            }
+        } else {
+            binding.apply {
+                btnRegister.isVisible = true
+                userDataView.isVisible = false
+                logOut.isVisible = false
+            }
         }
 
         binding.btnRegister.setOnClickListener {
@@ -89,13 +100,38 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-    private fun hideUserLogOut() {
-        val isUserLogin = isUserLoggedIn(this)
-        if (isUserLogin) {
-            binding.logOut.visibility = View.VISIBLE
-        } else {
-            binding.logOut.visibility = View.GONE
-        }
+    private fun getProfile() {
+        val retrofitInstance = RetrofitInstance(this).userApi.userProfile(user.token)
+
+        retrofitInstance.enqueue(object : Callback<Profile> {
+            override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                if (response.isSuccessful) {
+                    val profile = response.body()
+                    if (profile != null) {
+                        binding.userName.apply {
+                            text = profile.name
+                            if (profile.is_verified && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                tooltipText = context.getString(R.string.verified)
+                            } else {
+                                setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+                            }
+                        }
+
+                        binding.userMobile.text = user.mobile
+                        binding.userEmail.text = user.email
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Profile>, t: Throwable) {
+            }
+        })
+    }
+
+    override fun onStart() {
+        checkUser()
+        getProfile()
+        super.onStart()
     }
 
     private fun chooseThemeDialog() {
